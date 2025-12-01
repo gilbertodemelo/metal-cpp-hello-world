455ab048ad4439c5b5db6f21a9e1c50dce729f4b
Gilberto De Melo, Jr <gilberto.demelo@yahoo.com>
2025-12-01 20:22:34 -0300

    function to add vectors

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -5,12 +5,21 @@
 
 int main() {
 
-    int n = 100;
+    int n = 10;
     int min= 0;
-    int max = 10000;
+    int max = 10;
 
     int *a = generate_vector(n, min, max);
+    int *b = generate_vector(n, min, max);
+    int *c = new int[n];
 
+    add_vectors(a, b, c, n);
+    
+
+    // add vectors
+    for (int i = 0; i < n; i++) {
+        std::cout << a[i] << " + " << b[i] << " = " << c[i] << std::endl;
+    }
 
 
     return 0;
